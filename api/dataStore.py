f8b95f3a1a4b03a9f87fd68728f2dabd8e2b0cbd
koraypoyraz <koraypoyraz1@gmail.com>
2023-03-08 11:19:40 +0100

    added api

---
+++ api/dataStore.py
@@ -0,0 +1,47 @@
+import geopandas as gpd
+import pyproj
+from shapely import Point
+
+CRS = 4326
+
+stations = gpd.read_parquet('data/atom/stations.parquet')
+verzorgingsgebied = gpd.read_parquet('data/atom/verzorgingsgebied.parquet')
+
+stations = stations.to_crs(pyproj.CRS.from_epsg(CRS))
+verzorgingsgebied = verzorgingsgebied.to_crs(pyproj.CRS.from_epsg(CRS))
+
+
+def geo_to_point(lat, lon) -> Point:
+    return Point(lat, lon)
+
+
+def get_segment(lat, lon):
+    address = geo_to_point(lat, lon)
+
+    to_drop = ['peildatum']
+
+    destination_segment_idx = address.intersects(verzorgingsgebied.geometry)
+    destination_segments = verzorgingsgebied[destination_segment_idx]
+
+    return destination_segments.drop(to_drop, axis=1)
+
+
+def load_segment(lat, lon):
+    destination_segments = get_segment(lat, lon)
+    return destination_segments
+
+
+def load_station(lat, lon):
+    destination_segments = get_segment(lat, lon)
+
+    destination_station_idx = destination_segments.iloc[0, :].geometry.intersects(stations.geometry)
+    destination_station = stations[destination_station_idx]
+
+    return destination_station.geometry
+
+
+def load_segments():
+    cols = ['station', 'netbeheerder', 'status', 'beschikbareCapaciteitInvoedingHuidigMva', 'beschikbareCapaciteitAfnameHuidigMva',
+            'verwachtJaarVanOverbelastingInvoeding', 'verwachtJaarVanOverbelastingAfname', 'geometry']
+
+    return verzorgingsgebied[cols]
